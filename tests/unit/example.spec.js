128da3cde54bd9617fe3bb2f440c06c35d1dced3
Jacques Favreau <jacques@udacity.com>
2014-09-28 22:27:15 -0700

    Add(Testing): Add Karma and setup.

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,12 @@
+describe('The example spec',  function () {
+	'use strict';
+	beforeEach(function () {
+		//module('myAwesomeProject');
+	});
+
+	it('should just run', inject(
+		function () {
+			expect(true).toBeTruthy();
+		}
+	));
+});
